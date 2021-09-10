517ddfd34b6b88ccc37d4f7c260272c081f1b8c4
baltasar <baltaguirre@gmail.com>
2021-09-10 13:42:43 -0300

    (src/Components/Contact/Contact.jsx): add links

--- my-portfolio/src/Components/Contact/Contact.jsx
+++ my-portfolio/src/Components/Contact/Contact.jsx
@@ -8,16 +8,28 @@ import github from './github.png'
 
 
 export default function Contact(){
+    const gmailStyle = {
+        height:'100%',
+        marginLeft:'15%'
+    }
+    const githubStyle = {
+        height:'150%',
+        marginLeft:'15%'
+    }
+    const linkedinStyle = {
+        height:'170%',
+        marginLeft:'15%'
+    }
     return (
         <div className="containerContact">
             <Navbar/>
             <div className="contact">
                <a href="https://www.linkedin.com/in/baltasar-aguirre-fullstack/" target="blank">
-                    <img src={linkedin} alt="linkedin"/>LinkedIn</a>
+                    <img src={linkedin} alt="linkedin" style={linkedinStyle}/>LinkedIn</a>
                     <a href="https://github.com/Baltaguirre" target="blank">
-                    <img src={github} alt="linkedin"/>GitHub</a>
+                    <img src={github} alt="linkedin" style={githubStyle}/>GitHub</a>
                     <a  href="mailto:baltasar.aguirre@gmail.com" target="blank">
-                    <img src={gmail} alt="linkedin"/>Gmail</a>
+                    <img src={gmail} alt="linkedin" style={gmailStyle}/>Gmail</a>
             </div>
         </div>
     )
