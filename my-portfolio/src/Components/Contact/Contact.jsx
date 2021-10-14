8392410309f809f10521e859cbb6ba9e820f42fd
baltasar <baltaguirre@gmail.com>
2021-10-14 19:51:58 -0300

    (src/Components/Contact.jsx):add download resume link & img

--- my-portfolio/src/Components/Contact/Contact.jsx
+++ my-portfolio/src/Components/Contact/Contact.jsx
@@ -4,24 +4,32 @@ import './Contact.css';
 import linkedin from '../../public/Contact/linkedin.png';
 import gmail from '../../public/Contact/gmail.png';
 import github from '../../public/Contact/github.png'
-
-
+import cv from '../../public/Contact/CV Aguirre Baltasar EN.pdf'
+import cvpixel from '../../public/Contact/CV.png'
 
 export default function Contact({ button, playNavbar }){
     const gmailStyle = {
         height:'100%',
         width:'80%',
-        marginLeft:'6%'
+        marginLeft:'6%',
+        marginTop:'-20%'
     }
     const githubStyle = {
-        height:'130%',
-        width:'80%',
-        marginLeft:'5%'
+        height:'140%',
+        width:'70%',
+        marginLeft:'9%'
     }
     const linkedinStyle = {
-        height:'170%',
-        width:'80%',
-        marginLeft:'5%'
+        height:'120%',
+        width:'70%',
+        marginLeft:'9%'
+    }
+    const cvStyle = {
+        height:'230%',
+        width:'50%',
+        marginLeft:'25%',
+        marginBottom:'-5%',
+        marginTop:'-18%'
     }
     return (
         <div className="containerContact">
@@ -35,6 +43,8 @@ export default function Contact({ button, playNavbar }){
                     <img src={github} alt="github" style={githubStyle}/>GitHub</a>
                     <a  href="mailto:baltasar.aguirre@gmail.com" target="blank">
                     <img src={gmail} alt="g-mail" style={gmailStyle}/>Gmail</a>
+                    <a href={cv} download="CV Baltasar Aguirre" >
+                    <img src={cvpixel} alt="CV" style={cvStyle}/>Download Resume</a>
             </div>
         </div>
     )
